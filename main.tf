673688396aec765748426e69b83ebca2a30949ad
Sumeet <sumeet.ninawe@gmail.com>
2022-11-23 16:32:46 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ variable "name_tag" {
 }
 
 resource "aws_instance" "my_vm" {
-  count         = terraform.workspace == "default" ? 3 : 1
+  count         = var.workspace == "default" ? 3 : 1
   ami           = var.ami //Ubuntu AMI
   instance_type = var.instance_type
 
   tags = {
-    Name = format("%s_%s", var.name_tag, terraform.workspace)
+    Name = format("%s_%s", var.name_tag, var.workspace)
   }
 }
